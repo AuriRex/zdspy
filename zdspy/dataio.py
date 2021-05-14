8b084fc66ddfa8aeed61d74923a4559351803649
AuriRex <xaurirexx@gmail.com>
2021-05-14 22:09:57 +0200

    less copy + paste in dataio

--- zdspy/dataio.py
+++ zdspy/dataio.py
@@ -2,96 +2,61 @@
 import struct
 
 def ReadFile(filename):
+    file_content: bytearray
     with open(filename, "rb") as binary_file:
         # Read the whole file at once
-        return bytearray(binary_file.read())
+        file_content = bytearray(binary_file.read())
+    return file_content
 
-def Decode(data):
+def Decode(data: bytearray) -> str:
     return bytearray(reversed(data)).decode()
 
-def Encode(data):
+def Encode(data: str) -> bytearray:
     return bytearray(reversed(data.encode()))
 
-# Write UTF-8 String "string" into data "data" at offset "offset" with zeroed out length "length"
-def w_UTF8String(data, offset, length, string):
+# 
+def w_UTF8String(data: bytearray, offset: int, length: int, string: str) -> bytearray:
+    """Write the str `string` into the bytearray `data` at offset `offset` with length `length` using the UTF-8 encoding."""
+
     if len(string) > length:
-        print("[ERROR] String too long for valid space: "+str(length))
-        return bytearray(length)
+        print("[WARNING] String too long for valid space: "+str(length)+" - string truncated!")
+        string = string[:length]
     data[offset:offset+len(string)] = string.encode()
     return data
-    
 
-def UInt32(data, offset, islittleendian=True):
-    nums = data[offset: offset+4]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<I', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>I', nums)
-        return i
 
-def UInt16(data, offset, islittleendian=True):
-    nums = data[offset: offset+2]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<H', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>H', nums)
-        return i
+def _Int(data: bytearray, offset: int, is_little_endian: bool=True, number_of_bytes: int=1, is_signed: bool=True) -> int:
+    pack_fmt = _get_pack_fmt(number_of_bytes, is_little_endian, is_signed)
 
-def UInt8(data, offset, islittleendian=True):
-    nums = data[offset: offset+1]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<B', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>B', nums)
-        return i
+    nums = data[offset: offset+number_of_bytes]
 
-def SInt32(data, offset, islittleendian=True):
-    nums = data[offset: offset+4]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<i', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>i', nums)
-        return i
+    value, = struct.unpack(pack_fmt, nums)
 
-def SInt16(data, offset, islittleendian=True):
-    nums = data[offset: offset+2]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<h', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>h', nums)
-        return i
+    return value
 
-def SInt8(data, offset, islittleendian=True):
-    nums = data[offset: offset+1]
-    # print(nums)
-    if islittleendian:
-        # little endian
-        i, = struct.unpack('<b', nums)
-        return i
-    else:
-        # big endian
-        i, = struct.unpack('>b', nums)
-        return i
+def UInt32(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 4
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=False)
+
+def UInt16(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 2
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=False)
+
+def UInt8(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 1
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=False)
+
+def SInt32(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 4
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=True)
+
+def SInt16(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 2
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=True)
+
+def SInt8(data: bytearray, offset: int, is_little_endian: bool=True) -> int:
+    number_of_bytes = 1
+    return _Int(data, offset, is_little_endian, number_of_bytes, is_signed=True)
 
 
 def SFix32_16(data, offset, islittleendian=True):
@@ -159,7 +124,7 @@ def SFix16_8(data, offset, islittleendian=True):
         i, = struct.unpack('>b', nums)
         return i + f
 
-def SFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
+def SFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True) -> float:
 
     num_bytes = int(n_bits / 8)
     
@@ -169,7 +134,7 @@ def SFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
         # little endian
         nums = data[offset : offset + num_bytes][::-1]
 
-        f = 0
+        f: float = 0.0
 
         nums = str(bin(int(nums.hex(), base=16)))[2:].rjust(n_bits, "0")
         # print(nums)
@@ -194,7 +159,7 @@ def SFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
         # big endian
         nums = data[offset : offset + num_bytes]
 
-        f = 0
+        f: float = 0.0
 
         nums = str(bin(int(nums.hex(), base=16)))[2:].rjust(n_bits, "0")
         if nums[0] == "1":
@@ -211,7 +176,7 @@ def SFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
 
         return f
 
-def UFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
+def UFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True) -> float:
 
     num_bytes = int(n_bits / 8)
     
@@ -219,7 +184,7 @@ def UFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
         # little endian
         nums = data[offset : offset + num_bytes][::-1]
 
-        f = 0
+        f: float = 0.0
 
         power = n_bits_int-1
         for c in str(bin(int(nums.hex(), base=16)))[2:].rjust(n_bits, "0"):
@@ -230,7 +195,7 @@ def UFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
     else:
         # big endian
         nums = data[offset : offset + num_bytes]
-        f = 0
+        f: float = 0.0
 
         power = n_bits_int-1
         for c in str(bin(int(nums.hex(), base=16)))[2:].rjust(n_bits, "0"):
@@ -239,129 +204,51 @@ def UFix(data, offset, n_bits=32, n_bits_int=16 ,islittleendian=True):
 
         return f
 
-def w_UInt32(data, offset, newdata, islittleendian=True):
-    il = 4
+
+def _w_Int(data: bytearray, offset: int, value: int, is_little_endian=True, number_of_bytes=1, is_signed=True) -> bytearray:
+    pack_fmt = _get_pack_fmt(number_of_bytes, is_little_endian, is_signed)
+
+    packed_value = bytearray(struct.pack(pack_fmt, value))
+
+    for i in range(number_of_bytes):
+        data[offset+i] = packed_value[i]
+
+    return data
+
+def w_UInt32(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 4
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<I', newdata))
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=False)
 
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>I', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-
-def w_UInt16(data, offset, newdata, islittleendian=True):
-    il = 2
+def w_UInt16(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 2
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<H', newdata))
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=False)
 
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>H', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-
-def w_UInt8(data, offset, newdata, islittleendian=True):
-    il = 1
+def w_UInt8(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 1
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<B', newdata))
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=False)
 
-        for i in range(il):
-            data[offset+i] = bd[i]
 
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>B', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-
-def w_SInt32(data, offset, newdata, islittleendian=True):
-    il = 4
+def w_SInt32(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 4
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<i', newdata))
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=True)
 
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>i', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-
-def w_SInt16(data, offset, newdata, islittleendian=True):
-    il = 2
+def w_SInt16(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 2
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<h', newdata))
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=True)
 
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>h', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-
-def w_SInt8(data, offset, newdata, islittleendian=True):
-    il = 1
+def w_SInt8(data: bytearray, offset: int, value: int, is_little_endian: bool=True) -> bytearray:
+    number_of_bytes = 1
     data = bytearray(data)
-    if islittleendian:
-        # little endian
-        bd = bytearray(struct.pack('<b', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
-    else:
-        # big endian
-        bd = bytearray(struct.pack_into('>b', newdata))
-
-        for i in range(il):
-            data[offset+i] = bd[i]
-
-        return data
+    return _w_Int(data, offset, value, is_little_endian, number_of_bytes, is_signed=True)
 
 # https://stackoverflow.com/questions/32675679/convert-binary-string-to-bytearray-in-python-3
-def _bitstring_to_bytes(s, islittleendian=True):
-    if islittleendian:
+def _bitstring_to_bytes(s: str, is_little_endian: bool=True) -> bytearray:
+    if is_little_endian:
         return int(s, 2).to_bytes(len(s) // 8, byteorder='little')
     else:
         return int(s, 2).to_bytes(len(s) // 8, byteorder='big')
@@ -583,17 +470,26 @@ def w_UFix(data, offset, newdata, n_bits=32, n_bits_int=16 ,islittleendian=True)
 
     return data
 
-# s = "OneNice"
-# buffer = bytearray(24)
-# print(w_UTF8String(buffer, 4 , 16 , s).hex())
-def _ones(num):
+
+_NUMBER_OF_BYTES_TO_STRUCT_CHARACTER = {
+    1: "B",
+    2: "H",
+    4: "I"
+}
+
+def _get_pack_fmt(number_of_bytes: int, is_little_endian: bool, is_signed: bool) -> str:
+    endianess_symbol = "<" if is_little_endian else ">"
+    pack_fmt = endianess_symbol + (_NUMBER_OF_BYTES_TO_STRUCT_CHARACTER[number_of_bytes].lower() if is_signed else _NUMBER_OF_BYTES_TO_STRUCT_CHARACTER[number_of_bytes])
+    return pack_fmt
+
+def _ones(num) -> str:
     s = str(num)
     s = s.replace("1", "_")
     s = s.replace("0", "1")
     s = s.replace("_", "0")
     return s
 
-def _twos(num, reverse=False):
+def _twos(num, reverse=False) -> str:
     s = str(num)
     size = len(s)
 
@@ -610,9 +506,40 @@ def _twos(num, reverse=False):
             s = (str(bin(int(s, 2)-1))[2:]).rjust(size, "0")
     if len(s) > size:
         return s[len(s)-size:]
-        # raise ValueError("todo") #TODO
-    # print(int(s2) & b'111111')
     return s
 
 if __name__ == "__main__":
-    print(_twos("110011"))
+    #print(_twos("110011"))
+    import old_dataio as od
+
+    datanew = bytearray(7)
+    dataold = bytearray(7)
+
+    print(175)
+    datanew = w_UInt8(datanew, 0, 175, False)
+    dataold = od.w_UInt8(dataold, 0, 175, False)
+
+    print(48830)
+    datanew = w_UInt16(datanew, 1, 48830, False)
+    dataold = od.w_UInt16(dataold, 1, 48830, False)
+
+    print(-305419896)
+    datanew = w_SInt32(datanew, 3, -305419896, True)
+    dataold = od.w_SInt32(dataold, 3, -305419896, True)
+
+    print(datanew.hex())
+    print(dataold.hex())
+
+    print("1: " + UInt8(datanew, 0, False).__str__())
+    print("1: " + od.UInt8(dataold, 0, False).__str__())
+
+    print("2: " + UInt16(datanew, 1, False).__str__())
+    print("2: " + od.UInt16(dataold, 1, False).__str__())
+
+    print("4: " + SInt32(datanew, 3, True).__str__())
+    print("4: " + od.SInt32(dataold, 3, True).__str__())
+    
+    #48830
+    #3452816845
+    #305419896
+    
